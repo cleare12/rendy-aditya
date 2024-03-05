4d48e1d54c36a430730fb1b004799cf0317426d1
cleare12 <162069452+cleare12@users.noreply.github.com>
2024-03-05 01:00:36 +0000

    Add files via upload

---
+++ kasir/admin/ringkasan.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>{{ product.name }}</title>
+  </head>
+  <body>
+  <form class="text-center needs-validation" method="POST">
+    <img class="mb-1" src="logo10.jpg" alt="" width="160" height="150">
+  <h1>Yunani era</h1>
+    <h2>Description:</h2>
+    <p>Sejarah Yunani meliputi sejarah wilayah yang kini merupakan bagian dari negara Yunani, dan juga sejarah bangsa Yunani dan wilayah yang pernah mereka kuasai. Persebaran permukiman Yunani berubah-ubah seiring berjalannya waktu.
+
+Suku yang berbahasa (proto-) Yunani pertama, yang kemudian dikenal dengan nama Mykenai, diduga tiba di daratan Yunani pada akhir abad ke-3 SM dan awal pertengahan abad ke-2 SM - kemungkinan sekitar tahun 1900 hingga 1600 SM.[1] Sebelumnya, wilayah tersebut dihuni oleh penduduk asli yang tidak berbahasa Yunani dan bertani.[2]
+
+Pada puncak kejayaannya, peradaban Yunani membentang dari Yunani hingga Mesir hingga pegunungan Hindu Kush di Afganistan. Semenjak itu, minoritas Yunani bermukim di bekas wilayah Yunani (seperti Turki, Albania, Italia, dll), dan emigran Yunani telah berasimilasi dengan berbagai masyarakat di dunia (seperti Amerika Serikat, Australia, dll). Kini sebagian besar orang Yunani tinggal di negara modern Yunani dan Siprus.</p>
+<form class="text-center needs-validation" method="POST">
+    <img class="mb-1" src="logo9.jpg" alt="" width="160" height="150">
+<h1>SCOUTING FOR BOYS</h2>
+<h2>Description:</h2>
+<p>Buku ini telah mengilhami jutaan pramuka di seluruh dunia, termasuk Indonesia. Salah satu buku yang wajib dibaca, tak hanya oleh anggota pramuka, tapi juga oleh para guru pendidik, pencinta alam, relawan kemanusiaan, hingga orangtua. Sarat pesan moral dan penuh dengan how-to kepramukaan yang masih relevan dengan kebutuhan pramuka zaman now.” —Dr. H. Adhyaksa Dault, S.H., M.Si., Ketua Kwarnas Gerakan Pramuka 2013 – 2018, Ketua Umum Wadah Pencinta Alam Vanaprastha.
+
+“Sejarah kepramukaan dunia tak bisa dipisahkan dari sejarah Baden-Powell. Sebagai Bapak Pramuka Dunia, pemikiran dan pengalaman beliaulah yang melandasi pendirian dan pengembangan sistem pendidikan kepramukaan di seluruh dunia.” — Prof. Dr. Suyatno, M.Pd, Kepala Pusdiklatnas Gerakan Pramuka, Guru Besar Universitas Negeri Surabaya (Unesa).
+
+“Buku ini ditulis dengan semangat Peter Pan dan Kim (novel legendaris) karya Kipling. Permainan, drama, dan kisah api unggunnya penuh dengan ide-ide cemerlang dan menyenangkan. Bab-bab tentang pelacakan, kerajinan kayu, berkemah, dan sikap kesatria penuh dengan informasi praktis dan latihan yang mengajarkan anak-anak bagaimana menjadi setia, sopan, dan inovatif.” —Los Angeles Times.</p>
+<form class="text-center needs-validation" method="POST">
+    <img class="mb-1" src="logo8.jpg" alt="" width="160" height="150">
+<h1>Max havelaar</h1>
+<h2>Description:</h2>
+<p>Buku yang bercerita tentang sistem tanam paksa yang diterapkan pemerintah kolonial Belanda di daerah Lebak, Banten, ini menjadi satu buku terpenting dalam sejarah sastra Belanda, bahkan saat ini menjadi bacaan wajib di sekolah-sekolah Belanda.</p>
+<form class="text-center needs-validation" method="POST">
+    <img class="mb-1" src="logo7.png" alt="" width="160" height="150">
+<h1>Psychology of Money</h1>
+<h2>Description:</h2>
+<p>Buku The Psychology of Money memuat 19 cerita pendek yang membahas dan mengupas berbagai hal terkait uang dan aspek-aspek kehidupan. Mengatur uang lebih dari sekadar memikirkan soal kekayaan. Meski kadang perkara uang bisa membuat kita stres dan tertekan, tapi dengan mencoba untuk memahami kebutuhan dan perkembangan dunia dari waktu ke waktu, kita bisa lebih bijak dalam memaknai uang.
+
+Tiap cerita dalam buku ini menghadirkan sudut pandang unik dan menarik terkait uang. Wawasan kita akan diperluas dengan memahami kompleksitas dunia yang kita tinggali, sekaligus membuat kita lebih mudah menerima realitas betapa dinamisnya kehidupan yang kita jalani ini. Kita pun akan disuguhi dengan berbagai data, kasus-kasus penting di dunia perekonomian, pertumbuhan dan perubahan ekonomi dari waktu ke waktu, hingga perspektif soal kebahagiaan. The Psychology of Money cocok dibaca oleh siapa saja yang ingin lebih bijak dan terarah dalam mengatur keuangan hingga bagi yang ingin menemukan makna-makna baru untuk menjalani hidup yang lebih sejahtera.</p>
+<form class="text-center needs-validation" method="POST">
+    <img class="mb-1" src="logo6.jpg" alt="" width="160" height="150">
+<h1>The Anunaki<h1>
+<h2>Description:</h2>
+<p>Anunnaki adalah seorang dewa yang dalam mitologi Sumeria, Akkadia, Asiria, dan Babilonia.[1] Terdapat berbagai keterangan mengenai jumlah Anunnaki dan hakikat mereka, tetapi keterangan-keterangan ini sering kali saling bertentangan. Dalam tulisan-tulisan Sumeria (yang berasal dari zaman Pasca-Akkadia), Anunnaki adalah dewa-dewa terkuat keturunan An (dewa langit) yang menentukan nasib umat manusia.
+
+Dalam kisah Turunnya Inanna ke Dunia Bawah, Anunnaki digambarkan sebagai tujuh hakim yang duduk di hadapan takhta Ereshkigal di Dunia Bawah untuk menguji kecerdasan mereka siapa yang layak memimpin bumi. Naskah-naskah Akkadia (seperti Epos Gilgames) kemudian juga menuliskan keterangan yang serupa. Pada zaman Babilonia Kuno, Anunnaki diyakini merupakan dewa-dewa Dunia Bawah, sementara para dewa langit dikenal dengan sebutan Igigi. Orang Het kuno menganggap Anunnaki sebagai generasi dewa tertua yang telah digulingkan dan di sembunyikan ke Dunia Bawah oleh para dewa yang lebih muda. Anunnaki sering muncul dalam buku sejarah semu modern (seperti buku-buku karya Zecharia Sitchin) dan dalam teori konspirasi (seperti yang dicetuskan oleh David Icke).</p>
+<form class="text-center needs-validation" method="POST">
+    <img class="mb-1" src="logo5.jpg" alt="" width="160" height="150">
+<h1>Principles of Power<h1>
+<h2>Description:</h2>
+<p>Deskripsi Buku
+Menjadi sosok yang berkuasa dan berpengaruh adalah keinginan banyak orang. Manusia merasa tidak berdaya ketika mereka tidak memiliki kuasa sehingga mereka akan melakukan apa pun untuk mendapatkan kekuasaan, atau setidaknya menjadi orang yang berpengaruh dan disegani. Kehidupan sehari hari menunjukkan bahwa mereka yang memiliki posisi tinggi cenderung lebih mudah hidupnya. Orang-orang menghormati mereka. Mereka mendapatkan prioritas pertama, serta pelayanan yang jauh lebih istimewa. Siapa yang tidak ingin?
+
+Kekuasaan memang terlalu menggoda untuk bisa ditolak. Sepanjang sejarah peradaban manusia, kita sudah membaca, melihat, mendengar, bahkan menyaksikan kekuasaan bergantian tumbuh dan jatuh. Intrik dan perebutan kekuasaan mewarnai hampir setiap pusat kekuasaan di seluruh dunia. Mulai dari istana kuno di Tiongkok dan Jepang, aula-aula megah di Romawi dan Yunani, gedung-gedung pemerintahan di Jerman dan London, istana-istana mewah di Prancis dan Italia, hingga padang rumput di Asia Tengah; semuanya menjadi saksi perebutan dan pergolakan kekuasaan.
+
+Buku ini bukanlah sekadar bacaan biasa,melainkan sebuah panduan praktis untuk "menguasai dan memanipulasi" orang lain dengan cara yang positif. Terdapat 33 kiat brilian di dalamnya yang akan membantu pembaca untuk membangun rasa hormat dari bawahan, mendapat perhatian dari atasan tanpa harus menjilat, dan bahkan bertahan dan meraih kesuksesan dalam persaingan yang ketat. Tidak hanya itu, buku ini juga mengajarkan bagaimana memanfaatkan musuh atau saingan agar menjadi sekutu yang mendukung kesuksesan kita.
+
+Dengan gaya bahasa yang sederhana namun efektif, buku ini membahas garis besar hukum kekuasaan dan memberikan tips praktis yang dapat diterapkan dalam kehidupan sehari-hari. Ditambah dengan kalimat motivasi dari para tokoh terkenal dunia dan kisah-kisah inspiratif dari berbagai masa, buku ini akan menginspirasi pembaca untuk menjadi pribadi yang powerful dan mampu mengatasi setiap tantangan yang dihadapinya.
+
+Setelah membaca buku ini, pembaca akan memiliki kemampuan untuk menguasai situasi dan menjadi sosok pribadi yang tak terkalahkan oleh orang lain maupun keadaan. Jangan lewatkan kesempatan untuk membaca buku ini dan mengubah hidupmu menjadi lebih baik! Siapkan dirimu untuk menjadi yang terbaik dengan mempelajari buku ini!</p>
+    </ul>
+  </body>
+</html>
